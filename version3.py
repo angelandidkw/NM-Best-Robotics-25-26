bded4071c31881e5b118d9e3a05d198778907f68
Angel <148724145+angelandidkw@users.noreply.github.com>
2025-10-21 21:38:46 -0500

    Add VEX robot configuration and control logic
    This file contains the configuration and control logic for a VEX robot, including motor and servo setup, random seed initialization, and control handling based on controller input.

---
+++ version3.py
@@ -0,0 +1,87 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+import math
+
+
+# Brain should be defined by default
+brain=Brain()
+
+
+# Robot configuration code
+motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+servo_a = Servo(brain.three_wire_port.a)
+
+
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+
+
+# Make random actually random
+def initializeRandomSeed():
+   wait(100, MSEC)
+   random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+   urandom.seed(int(random))
+    
+# Set random seed
+initializeRandomSeed()
+
+
+
+
+def play_vexcode_sound(sound_name):
+   # Helper to make playing sounds from the V5 in VEXcode easier and
+   # keeps the code cleaner by making it clear what is happening.
+   print("VEXPlaySound:" + sound_name)
+   wait(5, MSEC)
+
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+
+#endregion VEXcode Generated Robot Configuration
+
+
+brain=Brain()
+controller=Controller()
+
+
+servo = Servo(brain.three_wire_port.a)
+servo.set_position(-50,DEGREES)
+
+
+while True:
+   axis2 = controller.axis3.position()  # -100..100
+   axis1 = controller.axis1.position()  # -100..100
+  
+   left_speed = axis2 + axis1   # Forward/back + left/right
+   right_speed = axis2 - axis1  # Forward/back - left/right
+  
+   # Apply deadband and control left motor
+   if abs(left_speed) < 5: # abs removes the negative before evalutating the code
+       motor_1.stop()
+   else:
+       motor_1.spin(FORWARD, left_speed, PERCENT)
+      
+   # Apply deadband and control right motor
+   if abs(right_speed) < 5:
+       motor_2.stop()
+   else:
+       motor_2.spin(FORWARD, right_speed, PERCENT)
+  
+   # Servo control
+   if controller.buttonB.pressing(): # Change servo position to 90 degrees
+       servo_a.set_position(40,DEGREES)
+   elif controller.buttonA.pressing(): # Change servo position to 0 degrees
+       servo_a.set_position(0,DEGREES)
+
+
+
